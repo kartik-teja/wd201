f63daa161d62d3b9360ab173fdc023b7d1f00453
Kartik <kartikteja343@gmail.com>
2023-12-24 10:15:30 +0530

    render

--- todo-app/app.js
+++ todo-app/app.js
@@ -2,8 +2,13 @@ const express = require("express");
 const app = express();
 const { Todo } = require("./models");
 const bodyParser = require("body-parser");
+const path = require("path");
 app.use(bodyParser.json());
 
+app.set("view engine", "ejs");
+
+// eslint-disable-next-line no-undef
+app.use(express.static(path.join(__dirname, "public")));
 app.get("/", function (request, response) {
   response.send("Hello World");
 });
