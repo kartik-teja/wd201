dec8b8385dbcdb94f3ada36fc6e18fa07b46d831
Kartik <kartikteja343@gmail.com>
2024-01-06 09:13:53 +0530

    Second fix

--- todo-app/migrations/20231229133632-create-user.js
+++ todo-app/migrations/20231229133632-create-user.js
@@ -1,5 +1,6 @@
 "use strict";
 /** @type {import('sequelize-cli').Migration} */
+
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable("Users", {
@@ -11,10 +12,6 @@ module.exports = {
       },
       firstName: {
         type: Sequelize.STRING,
-        allowNull: false,
-        validate: {
-          notNull: true,
-        },
       },
       lastName: {
         type: Sequelize.STRING,
@@ -23,10 +20,6 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
         unique: true,
-        validate: {
-          notNull: true,
-          isEmail: true,
-        },
       },
       password: {
         type: Sequelize.STRING,
--- todo-app/migrations/20231230050138-add-user-id-in-todos.js
+++ todo-app/migrations/20231230050138-add-user-id-in-todos.js
@@ -3,10 +3,17 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn("Todos", "userId", {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    queryInterface.addColumn("Todos", "userId", {
       type: Sequelize.DataTypes.INTEGER,
     });
-    await queryInterface.addConstraint("Todos", {
+
+    queryInterface.addConstraint("Todos", {
       fields: ["userId"],
       type: "foreign key",
       references: {
@@ -14,21 +21,16 @@ module.exports = {
         field: "id",
       },
     });
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn("Todos", "userId");
     /**
      * Add reverting commands here.
      *
      * Example:
      * await queryInterface.dropTable('users');
      */
+
+    await queryInterface.removeColumn("Todo", "userId");
   },
 };
